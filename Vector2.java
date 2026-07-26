92c02331f8ee0d75a6a95ec24ade69e7d525bd6c
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-07-25 21:05:54 -0700

    Vector2 to record

--- Vector2.java
+++ Vector2.java
@@ -1,13 +1,1 @@
-public class Vector2<A,B> {
-
-    public final A a;
-    public final B b;
-
-    public Vector2(A a, B b) {
-
-        this.a = a;
-        this.b = b;
-
-    }
-
-}
+public record Vector2<A, B>(A a, B b) {}
